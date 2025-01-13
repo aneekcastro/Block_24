84ff5a6d3f7c8b7c3cc348a9334501b771dff1bd
aneekcastro <aneekvcastro@gmail.com>
2025-01-13 16:38:23 -0600

    Finished?

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { puppyList } from './data.js';
+import './path-to-css.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppylist)
+  const [featPupId, setFeatPupId] = useState(null)
+  function handleClick() {
+    const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  }
   return (
-    <div>
-      {}
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+          key={puppy.id}
+          onClick={() => {
+            console.log("puppy id: ", puppy.id);  
+            setFeatPupId(puppy.id);  
+          }}
+        >
+          {puppy.name}
+        </p>
+        );
+      })}
+
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
